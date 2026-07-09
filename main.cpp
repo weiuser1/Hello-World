ab383caebe5bc96bca17019ca2cf2dec5807c3ab
weichuang <941571359@qq.com>
2026-07-09 17:05:33 +0800

    实现sort排序

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 void sort(int arr[], int size) {
-    
+	int tmp = 0;
+	for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
 int main() {
     int arr[] = {12, 4, 89, 43, 21, 78};
